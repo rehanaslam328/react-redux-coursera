3c2e2e8246241957c41e6d14b51725dbffcd8e3b
Rehan <rabscs@gmail.com>
2020-09-30 22:05:12 +0500

    MenuCompnent Created

---
+++ src/components/MenuComponent.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+class MenuComponent extends Component {
+    state = {}
+    render() {
+        return (<h2>dsasd</h2>);
+    }
+}
+
+export default MenuComponent;
